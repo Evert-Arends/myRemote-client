5555410f058f520016771d4511a9b98e3a492506
Evert-Arends <evert-arends@hotmail.nl>
2015-12-02 15:08:54 +0100

    Worning on better code, working version.

--- myRemote.py
+++ myRemote.py
@@ -1,65 +1,56 @@
+#!/usr/bin/env python
+
 #File written by Evert Arends, all rights reserverd. First run: Thursday November 26 in 2015 around 3 PM.#
 import os, os.path
 import platform
 import urllib2
 import base64
 import sys
-#Check if keyfile exsitst#
-hd = os.path.expanduser('~')
-dir = os.path.expanduser('~/.myRemote/')
-fl = os.path.expanduser('~/.myRemote/user.kb')
-if not os.path.exists(dir):
-    os.mkdir(os.path.join(hd, '.myRemote'))
-    print 'Created dir: .myRemote'
 
-#Url variables#
-ip = 'http://programmeerbazen.nl/ip.php' #Returns IP
-get = 'get.php?M=' #Returns key if positive
-data= 'data.php?data=' #Inserts information
-#base = 'https://baseurl.com/Stub/For/App' #Base URL, every url that needs this gets included (baseurl + var)
-message = 'msg.php?M=' #Check if there is a message availible (Url var)
-command = 'cmd.php?M=' #Gets the command from DB(Urls var)
-register = 'reg.php?K=' #Registers inserted key(Urls var)
-#!Url variables#
+CONFIG_DIR = os.path.expanduser('~/.myRemote')
+IP = 'http://programmeerbazen.nl/ip.php' #Returns IP
+P_GET = 'get.php?M=' #Returns key if positive
+P_DATA = 'data.php?data=' #Inserts information
+P_BASE = 'https://myremote.io/panel/stub/' #Base URL, every url that needs this gets included (baseurl + var)
+P_MESSAGE = 'msg.php?M=' #Check if there is a message availible (Url var)
+P_COMMAND = 'cmd.php?M=' #Gets the command from DB(Urls var)
+P_REGISTER = 'reg.php?K=' #Registers inserted key(Urls var)
 
-#Reading the key, and if it does not exists asks for a new one.
-try:
-    fp = open(fl)
-    key = fp.read(1000) #max lines to read.
-    if(not key):
-        print 'Empty? please delete your .myRemote folder in your home/appdata folder.', key
-        sys.exit(42)
-except IOError:
-    key = raw_input("Key?")
-    fp = open(fl, 'w+')
-    print 'empty'
-    fp.write(key)
+def request():
     try:
         from urllib.request import urlopen
     except ImportError:
-        from  urllib2 import urlopen
-    s = urlopen(base + register + key)
-    print base + register + key
-fp.close()
-print 'succes!'
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
-s = urlopen(ip)
-pcName = platform.uname()[1]
-osName = platform.system()
-pip = s.read()
-#Encoding sData into a base64 string, so I can post spaces and weird characters.
-sData = base64.b64encode(key + ',' + pcName + ',' + osName + ',' + pip)
-#sData = base64.b64encode(sData)
-data = data + sData
-print data
-print base64.b64decode(data)
-#Request URL with a get parameter, which makes it easier to store the data on the serverself.
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
-#s = urlopen(base + data)
-print base + data
+        from urllib2 import urlopen
+    if not os.path.exists(CONFIG_DIR):
+        os.mkdir(CONFIG_DIR)
+        print('Created {0}'.format(CONFIG_DIR))
+
+    key_file = '{0}/user.kb'.format(CONFIG_DIR)
+    if os.path.exists(key_file):
+        key = open(key_file, 'r').read()
+        if len(key) == 0:
+            print('Key is empty, please delete ~/.myRemote and try again')
+            sys.exit(1)
+    else:
+        key = raw_input("Key?")
+        open(key_file, 'w+').write(key)
+        url = '{0}{1}{2}'.format(P_BASE, P_REGISTER, key)
+        s = urlopen(url)
+        print(url)
+        print('Success!')
+
+    s = urlopen(IP)
+    pcName = platform.uname()[1]
+    osName = platform.system()
+    pip = s.read()
+    #Encoding sData into a base64 string, so I can post spaces and weird characters.
+    sData = base64.b64encode(key + ',' + pcName + ',' + osName + ',' + pip)
+    #Request URL with a get parameter, which makes it easier to store the data on the serverself.
+    P_URL = '{0}{1}{2}'.format(P_BASE, P_DATA, sData)
+    s = urlopen(P_URL)
+    s = s.read()
+    print(s)
+
+if __name__ == '__main__':
+    request()
+
