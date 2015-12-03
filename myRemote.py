02cccfe5956bd5e24bf5e846db9e9f2c7b6491d6
Evert-Arends <evert-arends@hotmail.nl>
2015-12-03 14:41:37 +0100

    Worning API connection and response.

--- myRemote.py
+++ myRemote.py
@@ -7,20 +7,24 @@ import urllib2
 import base64
 import sys
 
-CONFIG_DIR = os.path.expanduser('~/.myRemote')
+CONFIG_DIR = os.path.expanduser('~/.myRemote') #Path where data is stored
 IP = 'http://programmeerbazen.nl/ip.php' #Returns IP
 P_GET = 'get.php?M=' #Returns key if positive
 P_DATA = 'data.php?data=' #Inserts information
-P_BASE = 'https://website.com/stub/' #Base URL, every url that needs this gets included (baseurl + var)
+P_BASE = 'PATH_TO_API' #Base URL, every url that needs this gets included (baseurl + var)
 P_MESSAGE = 'msg.php?M=' #Check if there is a message availible (Url var)
 P_COMMAND = 'cmd.php?M=' #Gets the command from DB(Urls var)
 P_REGISTER = 'reg.php?K=' #Registers inserted key(Urls var)
-
-def request():
-    try:
-        from urllib.request import urlopen
-    except ImportError:
-        from urllib2 import urlopen
+#Global most used vars
+G_OSNAME = platform.system() #Operating system's name
+G_PCNAME = platform.uname()[1] #Computername
+#Global functions
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+#!Global functions
+def filecheck():
     if not os.path.exists(CONFIG_DIR):
         os.mkdir(CONFIG_DIR)
         print('Created {0}'.format(CONFIG_DIR))
@@ -31,25 +35,43 @@ def request():
         if len(key) == 0:
             print('Key is empty, please delete ~/.myRemote and try again')
             sys.exit(1)
+        request_me()
     else:
         key = raw_input("Key?")
         open(key_file, 'w+').write(key)
         url = '{0}{1}{2}'.format(P_BASE, P_REGISTER, key)
         s = urlopen(url)
-        print(url)
-        print('Success!')
-
+        request()
+        print('Succesvol geregistreerd. (32 - 44)')
+def request():
+    key = '{0}/user.kb'.format(CONFIG_DIR)
+    f = open(key)
+    data = f.readline()
+    f.close()
+    print data
     s = urlopen(IP)
-    pcName = platform.uname()[1]
-    osName = platform.system()
     pip = s.read()
     #Encoding sData into a base64 string, so I can post spaces and weird characters.
-    sData = base64.b64encode(key + ',' + pcName + ',' + osName + ',' + pip)
+    sData = base64.b64encode(data + ',' + G_PCNAME + ',' + G_OSNAME + ',' + pip)
     #Request URL with a get parameter, which makes it easier to store the data on the serverself.
     P_URL = '{0}{1}{2}'.format(P_BASE, P_DATA, sData)
     s = urlopen(P_URL)
     s = s.read()
     print(s)
-
+    request_me()
+def request_me():
+    data = '{0}/user.kb'.format(CONFIG_DIR)
+    f = open(data)
+    key = f.readline()
+    f.close()
+    import threading
+    threading.Timer(5.0, request_me).start()
+    c = urlopen(P_BASE + P_GET + key)
+    str = c.read()
+    if key in str:
+        c = urlopen(P_BASE + P_MESSAGE + key)
+        c = c.read()
+        if "1" in c:
+            print('Het werkt :D :D :D')
 if __name__ == '__main__':
-    request()
+    filecheck()
