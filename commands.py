cf558a6f53193ef3e9879ae00c972d1b9f5c19de
Evert Arends <evert-arends@hotmail.nl>
2016-01-19 16:14:23 +0100

    Working screenshot fuction.

--- commands.py
+++ commands.py
@@ -1,6 +1,10 @@
+from time import sleep
 import webbrowser
+import requests
 import gtk.gdk
-from imgurpython import ImgurClient
+import wx
+import base64
+import constants as cfg
 
 
 class Commands():
@@ -18,34 +22,69 @@ class Commands():
         webbrowser.open(url)
         print(url)
 
+    def test(self):
+        with open("zijn.png", "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            print encoded_string
+
     def take_screenshot(self):
-        # w = gtk.gdk.get_default_root_window()
-        # sz = w.get_size()
-        # print "The size of the window is %d x %d" % sz
-        # pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, sz[0], sz[1])
-        # pb = pb.get_from_drawable(w, w.get_colormap(), 0, 0, 0, 0, sz[0], sz[1])
-        #
-        # ts = time.time()
-        # filename = "screenshot"
-        # filename += str(ts)
-        # filename += ".png"
-        #
-        # if (pb != None):
-        #     pb.save(filename, "png")
-        #     print "Screenshot saved to " + filename
-        # else:
-        #     print "Unable to get the screenshot."
+        print(cfg.G_OSNAME)
+        if cfg.G_OSNAME == "Linux" or cfg.G_OSNAME == "Linux2":
+            print('Screenshot requested.')
+            w = gtk.gdk.get_default_root_window()
+            sz = w.get_size()
+            print "The size of the window is %d x %d" % sz
+            pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
+            pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
 
-#testen van IMGUR API deze details kloppen wel!
-        client_id = '893455c3af30d48'
-        client_secret = '8e6f8b5e5f23288e0c6e9dcc65407b31c68c79b1'
+            filename = "screenshot"
+            filename += ".png"
 
-        client = ImgurClient(client_id, client_secret)
+            if (pb != None):
+                pb.save(filename,"png")
+                print "Screenshot saved to "+filename
+                sleep(0.50)
+                self.encode_image()
+            else:
+                print "Unable to get the screenshot."
+        else:
+            wx.App()  # Need to create an App instance before doing anything
+            screen = wx.ScreenDC()
+            size = screen.GetSize()
+            bmp = wx.Bitmap(size[0], size[1])
+            mem = wx.MemoryDC(bmp)
+            mem.Blit(0, 0, size[0], size[1], screen, 0, 0)
+            mem.SelectObject(wx.NullBitmap)
+            bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_JPEG)
+            print('Bitmap saved')
+            sleep(0.50)
+            self.encode_image()
 
-        # Example request
-        items = client.gallery()
-        for item in items:
-            print(item.link)
+
+    def encode_image(self):
+        with open("tux.png", "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            full_encoded_string = "data:image/png;base64,{0}".format(encoded_string)
+        encoded_string_file = '{0}/img.kb'.format(cfg.CONFIG_DIR)
+        try:
+            open(encoded_string_file, 'w+').write(full_encoded_string)
+            sleep(0.50)
+            print('Saved succesfully.')
+            self.upload_image()
+        except:
+            print('pr0bz brother.. No base64 image string for you :3')
+
+
+    def upload_image(self):
+        encoded_string_file = '{0}/img.kb'.format(cfg.CONFIG_DIR)
+        data = open(encoded_string_file, 'r').readline()
+        sleep(0.50)
+        print(data)
+        userdata = {'base64_str': data}
+        resp = requests.post('http://myremote-bermdingetje.c9users.io/stub/upload.php', data=userdata)
+        print(resp.status_code)
+        print('send')
+
 
 
 
--- constants.py.example.py
+++ constants.py.example.py
@@ -5,7 +5,7 @@ import platform
 # All Paths to use API
 CONFIG_DIR = os.path.expanduser('~/.myRemote') # Path where data is stored
 PY_VERSION = sys.version_info[0]
-MY_IP = ' http://programmeerbazen.nl/ip.php' # Returns IP
+MY_IP = ' 127.0.0.1' # Returns IP
 P_GET = 'get.php?M=' # Returns key if positive
 P_DATA = 'data.php?data=' # Inserts information
 P_BASE = 'https://example.com/API/' # Base URL, every url that needs this gets included (baseurl + var)
